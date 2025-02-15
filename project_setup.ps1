4b962ebfe508bb30cbc79df2380592043a08a01e
Michael Lohr <michael@lohr.dev>
2025-02-15 10:05:02 +0100

    feat: add raygui to setup script

--- project_setup.ps1
+++ project_setup.ps1
@@ -23,6 +23,7 @@ pub fn build(b: *std.Build) !void {
     });
 
     const raylib = raylib_dep.module("raylib");
+    const raygui = raylib_dep.module("raygui");
     const raylib_artifact = raylib_dep.artifact("raylib");
 
     //web exports are completely separate
@@ -50,6 +51,7 @@ pub fn build(b: *std.Build) !void {
 
     exe.linkLibrary(raylib_artifact);
     exe.root_module.addImport("raylib", raylib);
+    exe.root_module.addImport("raygui", raygui);
 
     const run_cmd = b.addRunArtifact(exe);
     const run_step = b.step("run", "Run $PROJECT_NAME");
