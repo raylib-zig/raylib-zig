943959c8717ac9a9cd986bc33fb6075d64b04520
Bd <49720243+bdnugget@users.noreply.github.com>
2026-05-10 21:15:15 +0200

    Fix emsdk.shell arg in project templates (raylib-zig#331) (#332)

--- project_setup.sh
+++ project_setup.sh
@@ -53,7 +53,7 @@ pub fn build(b: *std.Build) !void {
             .optimize = optimize,
             .flags = emcc_flags,
             .settings = emcc_settings,
-            .shell_file_path = emsdk.shell(raylib_dep.builder),
+            .shell_file_path = emsdk.shell(raylib_dep),
             .install_dir = install_dir,
             .embed_paths = &.{.{ .src_path = "resources/" }},
         });
