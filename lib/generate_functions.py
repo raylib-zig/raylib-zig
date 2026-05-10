19364a7b6e3b04626f724d402b6a2d2ae637c60d
Jairo Correa <jn.j41r0@gmail.com>
2026-05-10 19:26:47 -0300

    Integrate code generation into build.zig

--- lib/generate_functions.py
+++ lib/generate_functions.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import re
+import argparse
 
 """
 Automatic utility for generating raylib function headers.
@@ -489,37 +490,24 @@ def parse_header(header_name: str, output_file: str, ext_file: str, prefix: str,
 
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Automatic utility for generating raylib function headers.")
+
+    parser.add_argument("--header-file", required=True)
+    parser.add_argument("--prelude-file", required=True)
+    parser.add_argument("--ext-prelude-file", required=True)
+    parser.add_argument("--output-file", required=True)
+    parser.add_argument("--ext-output-file", required=True)
+    parser.add_argument("--prefix", required=True)
+    parser.add_argument("--skip-after", default="#/never\\#")
+
+    args = parser.parse_args()
+
     parse_header(
-        "raylib.h",
-        "raylib.zig",
-        "raylib-ext.zig",
-        "RLAPI ",
-        "preludes/raylib-prelude.zig",
-        "preludes/raylib-ext-prelude.zig"
-    )
-    parse_header(
-        "raymath.h",
-        "raymath.zig",
-        "raymath-ext.zig",
-        "RMAPI ",
-        "preludes/raymath-prelude.zig",
-        "preludes/raymath-ext-prelude.zig"
-    )
-    parse_header(
-        "rlgl.h",
-        "rlgl.zig",
-        "rlgl-ext.zig",
-        "RLAPI ",
-        "preludes/rlgl-prelude.zig",
-        "preludes/rlgl-ext-prelude.zig",
-        "#if defined(RLGL_IMPLEMENTATION)\n"
-    )
-    parse_header(
-        "raygui.h",
-        "raygui.zig",
-        "raygui-ext.zig",
-        "RAYGUIAPI ",
-        "preludes/raygui-prelude.zig",
-        "preludes/raygui-ext-prelude.zig",
-        "#if defined(RAYGUI_IMPLEMENTATION)\n"
+        args.header_file,
+        args.output_file,
+        args.ext_output_file,
+        args.prefix,
+        args.prelude_file,
+        args.ext_prelude_file,
+        args.skip_after,
     )
